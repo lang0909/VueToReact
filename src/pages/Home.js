5c7b40db0e9ef7708a11c2cc2f9f5e52db4c458a
lang0909 <lang0909@ajou.ac.kr>
2020-03-21 22:20:22 +0900

    Home.vue ->react Home.js version1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
 
 class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            player_name: [],
+            playerName: '',
+            spid: '',
+        }
+    }
+
+    handleClicked(id){
+        this.setState({
+            spid: id
+        });
+    }
+
     render(){
         return(
             <div>
-                <h2>
-                    홈
-                </h2>
+                <div className="logo_cont">
+                    <img className="logo" src="../assets/background.jpg"></img>
+                </div>
+                <div className="search_cont">
+                    <input type="text" placeholder="선수이름" />
+                    <button type="button" className="search_button" onClick={}>검색</button>
+                </div>
+                {}
             </div>
         );
     }
