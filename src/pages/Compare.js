6b5356bc760b8a93b2ab864c75ba17fdb1444375
lang0909 <lang0909@ajou.ac.kr>
2020-03-21 17:40:51 +0900

    add navigation pages

---
+++ src/pages/Compare.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Compare extends Component{
+    render(){
+        return(
+            <div>
+                <h2>
+                    비교하기
+                </h2>
+            </div>
+        );
+    }
+}
+
+export default Compare;
---
+++ src/pages/Recommend.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Recommend extends Component{
+    render(){
+        return(
+            <div>
+                <h2>
+                    포메이션 추천
+                </h2>
+            </div>
+        );
+    }
+}
+
+export default Recommend;
