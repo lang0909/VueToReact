72862576f43f17724fef0887172ead546164bdc7
lang0909 <lang0909@ajou.ac.kr>
2020-03-21 22:26:38 +0900

    Compare.vue -> react Compare.js version1

--- src/pages/Compare.js
+++ src/pages/Compare.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
 
 class Compare extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            playerName: '',
+            player_name: [],
+            playerkeep: [],
+            compare_content: [],
+            compare_content1: '',
+            compare_content2: '',
+        }
+    }
+
     render(){
         return(
             <div>
-                <h2>
-                    비교하기
-                </h2>
+                <div className="logo_cont">
+                    <img className="logo" src="../assets/background.jpg" />
+                </div>
+                <div className="search_cont">
+                    <input type="text" className="search_playerName" placeholder="선수이름" />
+                    <button type="button" className="search_button" onClick={}>검색</button>
+                </div>
+                {}
+                {}
+                {}
             </div>
         );
     }
