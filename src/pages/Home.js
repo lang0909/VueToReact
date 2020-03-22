86e14559e0acf6e70cbb3fbdf41333d7f852d564
lang0909 <lang0909@ajou.ac.kr>
2020-03-22 13:41:51 +0900

    add search result component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,6 +16,35 @@ class Home extends Component{
         });
     }
 
+    handleSearchResult(array){
+        let arr =[];
+        for(i=0;i<array.length;i++){
+            arr.push(
+                <span>
+                    <span>
+                        <img 
+                            src={require(`/players/${array[i].id}`)}
+                            className="back_img"
+                            style={{backgroundImage=require(`/season_background/${array[i].id.substring(0,3)}.png`)}}>
+                        </img>
+                    </span>
+                    <div className="name_cont">
+                        <span>
+                            <img
+                                src={require(`/season/${array[i].id.substring(0,3)}.JPG`)} className="img_cont">
+                            </img>
+                            {array[i].name}
+                        </span>
+                    </div>
+                    <div>
+                        <button type="button" value={array[i].id} onClick={} className="player_btn">선택</button>
+                    </div>
+                </span>
+            );
+        }
+        return arr;
+    }
+
     render(){
         return(
             <div>
@@ -26,7 +55,11 @@ class Home extends Component{
                     <input type="text" placeholder="선수이름" />
                     <button type="button" className="search_button" onClick={}>검색</button>
                 </div>
-                {}
+                <div>
+                    {this.state.player_name.length == 0 ?
+                    <></>:
+                    this.handleSearchResult(this.state.player_name)}
+                </div>
             </div>
         );
     }
